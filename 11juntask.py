7182f151ac3ec882408ffc2a86c7dd2c038d1629
faizankhan-dev <faizankhan70008@gmail.com>
2025-06-11 19:49:55 +0530

    hello

---
+++ 11juntask.py
@@ -0,0 +1,10 @@
+
+
+a=input("Enter a character to check =")
+fk=a.lower()
+if(fk=="a"or fk=="e"or fk=="i" or fk=="o" or fk=="u"):
+    print("VOWEL")
+
+else:
+    print("CONSONET")
+
---
+++ newtask.py
@@ -0,0 +1,19 @@
+# def addition():
+#   a=int(input("Enter the value of a :"))
+#   b=int(input("Enter the value of b :"))
+#   c=a+b
+#   print(c)
+# addition()
+
+
+def simple():
+  p=int(input("Enter the value of p :"))
+  r=int(input("Enter the value of r :"))
+  t=int(input("Enter the value of t :"))
+  SI=(p*r*t)/100
+  print(SI)
+simple()
+
+
+
+
